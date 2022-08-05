51a49f43f7e14743e10f55407bdb1ae6fe0e6c83
Leonardo <leonardooliveirarosadev@hotmail.com>
2022-08-04 22:42:07 -0300

    04082022

--- assets/js/arrays.js
+++ assets/js/arrays.js
@@ -17,3 +17,65 @@ A lista [1] (lista na posição 1) vai conter o valor "feijão".
 E assim por diante.
 
 */
+
+
+//  Essa forma a baixo nao é viavel!
+var item1 = "arroz";
+var item2 = "feijao";
+var item3 = "macarrao";
+var item4 = "leite";
+
+// Com a matriz fica muito mais facil apenas passando o indice
+//  Os indices ou posicoes sempre começam com 0
+// Isso chama arrya literal
+
+// Primeira forma de criar um array e mais utilizada
+
+//    indices       [0]      [1]       [2]       [3]
+// const lista = ["arroz", "feijão","macarrão","leite"];
+
+// Segunda forma de criar um array!
+
+// const lista = [];
+// lista[0] = "arroz";
+// lista[1] = "feijão";
+// lista[2] = "macarrão";
+// lista[3] = "leite";
+
+// Terceira forma de criar um array
+
+// const lista = new Array("arroz","feijão","macarrão","leite");
+
+// Tambem podemos alterar um item do array
+// lista[0]= "Ovo"
+
+// let x = lista[0];
+// console.log(x)
+
+// para poder ver todos os itens de uma matriz é só fazer assim !
+// console.log(lista)
+
+// const pessoa =["Leonardo","Oliveira", 41]; //Array
+// const pessoaa = {nome:"Leonardo", sobrenome:"Oliveira", idade: 41};//Objeto
+
+// pessoaa.nome;
+// pessoa[0];
+
+
+const pessoa =["Leonardo","Oliveira", 41,"estudante"]; 
+// alert(pessoa.length); // Quantos tem dentro do meu array 3
+
+// let ver = pessoa[0];
+// console.log(ver);
+
+// esse codigo nos mostra o ultimo item do nosso array
+// alert(pessoa[pessoa.length -1])
+
+// Como acrescentar mais um item dentro da nossa matriz usa o push ou o 
+//outro , melhor usar o push
+// pessoa.push("Brasileiro");
+// pessoa[pessoa.length] ="Casado"
+// console.log(pessoa)
+
+// Como saber se uma variável é uma matriz ou não é (ARRAY)
+alert(Array.isArray(pessoa));
